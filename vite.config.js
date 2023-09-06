053d05dcf037efdaaa9ecda38eff20c369a8fc51
AmEhlee <ElieKabengele.Work@gmail.com>
2023-09-05 21:39:08 -0600

    small update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/University-Lookup/",
+  base: "/university-lookup/",
   plugins: [react()],
 })
